358b77d60f882b157541399f9a57337717fe7cda
Yangming He <yangminghe20@gmail.com>
2024-07-12 01:52:57 +0800

    feat: 实现几个关于str的函数

--- src/utils.c
+++ src/utils.c
@@ -1,11 +1,44 @@
 #include "syscall.h"
 
-ssize_t strlen(const char *buf)
+ssize_t str_len(const char *buf)
 {
 	int len = 0;
 	while (buf[len] != '\0')
 		len++;
 	return len;
+}
+
+ssize_t str_cmp(const char *s1, const char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0') {
+		if (*s1 != *s2)
+			return 1;
+		s1++;
+		s2++;
+	}
+	if (*s1 == '\0' && *s2 == '\0')
+		return 0;
+	return 1;
+}
+
+void str_copy(char *dest, const char *src)
+{
+	while (*src != '\0') {
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+}
+
+ssize_t is_all_digit(char *buf)
+{
+	while (*buf != '\0') {
+		if (*buf < '0' || *buf > '9')
+			return 0;
+		buf++;
+	}
+	return 1;
 }
 
 ssize_t alloc_memory(size_t size)
--- src/utils.h
+++ src/utils.h
@@ -1,9 +1,13 @@
 #include "syscall.h"
 
-// 打印字符串
-#define log(x) write(STDOUT, x, strlen(x))
-
 // 计算字符串长度
-ssize_t strlen(const char *buf);
+ssize_t str_len(const char *buf);
+
+// 比较两个字符串是否相等 相等返回0 不相等返回1
+ssize_t str_cmp(const char *s1, const char *s2);
+
+void str_copy(char *dest, const char *src);
+
+ssize_t is_all_digit(const char *buf);
 
 ssize_t alloc_memory(size_t size);
